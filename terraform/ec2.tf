34ba2450d83c3028712f1a66014581241d3cf4aa
root <root@ip-172-31-54-255.ec2.internal>
2024-06-30 18:21:08 +0000

    code

---
+++ terraform/ec2.tf
@@ -0,0 +1,66 @@
+variable "private_key_path" {
+  description = "Path to the private key file"
+  type        = string
+}
+
+resource "aws_security_group" "strapi_sg" {
+  name        = "ashok-security-group"
+  description = "Security group for Strapi EC2 instance"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 1337
+    to_port     = 1337
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "strapi" {
+  ami           = "ami-04b70fa74e45c3917"  # Correct AMI ID for ap-south-1
+  instance_type = "t2.medium"              # Changed to t2.medium
+  key_name      = "Veera"                  # Your key pair name
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+
+  tags = {
+    Name = "StrapiServer"
+  }
+
+  provisioner "remote-exec" {
+  inline = [
+      "sudo apt-get update",
+      "curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -",
+      "sudo apt install -y nodejs",
+      "sudo npm install -g yarn",
+      "sudo apt-get install git -y",
+      "sudo npm install -g pm2 ",
+      #"sudo npm install -g strapi@latest -y ",
+  ]
+}
+
+
+    connection {
+      type        = "ssh"
+      user        = "ubuntu"
+      private_key = file(var.private_key_path)
+      host        = self.public_ip
+    }
+  }
+
+
+output "instance_ip" {
+  value = aws_instance.strapi.public_ip
+}
